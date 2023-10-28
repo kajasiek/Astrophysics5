ea5b66b9f7f1f5196d369c1601d7c88990659ace
kajasiek <133694957+kajasiek@users.noreply.github.com>
2023-10-28 13:58:19 +0200

    fuuuuuuuuuuuuuuk this shit

--- Task 3/Task 3.py
+++ Task 3/Task 3.py
@@ -1,49 +1,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import tplquad
-from scipy.optimize import root
-import sys
-import sympy as smp
 
 
-def ratio(m1, m2):
-    q = m2/m1
-    if(q > 1):
-        print("Wrong values")
-        sys.exit()
-    else:
-        return q
-
-#Constants 
-m1 = 2
-m2 = 1
-q = ratio(m1, m2)
-
-A1 = q/(1+q)
-A2 = 1/(1+q)
-
-'''
-Finding L1
-'''
-
-def phi1l1(x, q):
-    return 
-
-def phi1(x, y, z, q):
-    return -(1/(1+q))/np.sqrt((x + q/(1+q))**2 + y**2 + z**2)
-
-def phi2(x, y, z, q):
-    return -(q/(1+q))/np.sqrt((x - 1/(1+q))**2 + y**2 + z**2) 
-
-def phi3(x, y):
-    return -0.5 * (x**2 + y**2) 
+ratio = np.arange(0.01, 100, 0.1)
 
 
 
+plt.title("Hill/Roche sphere radius")
+plt.xlabel("q")
+plt.ylabel("RL")
+plt.text(20, 0.65, "Calculated manually", c='b')
+plt.text(20, 0.48, "Paczynski approximation", c='r')
+plt.text(20, 0.23, "Kopal approximation", c='g')
+for i, q in enumerate(ratio):
+    A = 2 * (1 + q)
+    B = - 4 * (1 + q)/(1 - q) + 4 * q 
+    C = 2 * (1 + q)/(1 - q)**2 + 8 * q/(1 - q) + 2 * q**2/(1 + q)
+    D = 4 * q/(1 - q)**2 + 4 * q**2/(1 - q**2) - q + 1
+    E = 2 * q**2/((1 + q) * (1 - q)**2) - 2 * q**2/(1 + q) - 2/(1 - q)
+    F = - q**3/(1 + q)**2 + 1/(1 - q)**2
+    
 
 
-fun = lambda z, y, x: 1 
-found = tplquad(fun, -1.136, 0.237, -0.8, 0.8, -0.8, 0.8)
-print(found)
 
+    sol = np.roots([A,B,C,D,E,F])
 
+    
+    x1 = -1/(1+q)
+    x2 = q/(1+q)
+    
+    R = np.abs((sol[2] - x1))
+    
+    f = lambda z, y, x: 1
+    
+    volume = tplquad(f, x1-R, sol[2], -R, R, -R, R)
+    rl = ((3 * volume[0])/(4 * np.pi))**(1/3)
+    plt.scatter(q, rl, c='b', s=5)
+
+rl1 = 2/3**(4/3) * (ratio/(1+ratio))**(1/3)
+rl2 = 2/3**(4/3) * (1+ratio)**(-1/3)
+plt.scatter(ratio, rl1, c='r', s=5)
+plt.scatter(ratio, rl2, c='g', s=5)
+plt.show()
